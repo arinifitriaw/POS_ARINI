e0dadde163f3ce186d6a2e54e9301e3141a4a42e
nadhirasapiraa-dot <nadhirasapiraa@gmail.com>
2026-09-04 10:35:51 +0700

    update database

--- database/migrations/2026_01_15_043342_create_penjualan_table.php
+++ database/migrations/2026_01_15_043342_create_penjualan_table.php
@@ -1,31 +1,29 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('penjualan', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users', 'id')->nullOnDelete();
-            $table->integer('total_pembayaran');
-            $table->string('metode_pembayaran')->nullable();
-            $table->enum('status',['OPEN', 'COMPLETED']);
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('penjualan');
-    }
+<?php 
+ 
+use Illuminate\Database\Migrations\Migration; 
+use Illuminate\Database\Schema\Blueprint; 
+use Illuminate\Support\Facades\Schema; 
+ 
+return new class extends Migration 
+{ 
+    public function up(): void 
+    { 
+        Schema::create('penjualan', function (Blueprint $table) { 
+            $table->id(); 
+            $table->foreignId('user_id')
+            ->nullable()
+            ->constrained('users', 'id')
+            ->nullOnDelete(); 
+            
+            $table->integer('total_pembayaran'); 
+            $table->string('metode_pembayaran')->nullable(); 
+            $table->enum('status', ['OPEN', 'COMPLETED']); 
+            $table->timestamps(); 
+        }); 
+    } 
+ 
+    public function down(): void 
+    { 
+        Schema::dropIfExists('penjualan'); 
+    } 
 };
